1610c692a646883f54db5bc83c9b276e85b0b448
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2020-10-24 20:22:05 +0200

    some work on the one-page-per-location feature

--- staticSiteGenerator.py
+++ staticSiteGenerator.py
@@ -76,7 +76,7 @@ logger.debug("Media: {0}".format(media))
 
 recordsToWrite = {}
 for record in media: # Loop through all records
-    if not record["location"] in recordsToWrite: # ... if we don't have the location (branch office)
+    if not record["location"] in recordsToWrite: # ... if we don't have the location (branch offices)
         recordsToWrite[record["location"]] = {} # ... add it do the dict
 
     if not record["category"] in recordsToWrite[record["location"]]: # now we do the same with the categories
@@ -84,6 +84,7 @@ for record in media: # Loop through all records
 
 logger.debug("Records: {0}".format(recordsToWrite))
 
+# Reverse Locations as a quick fix for issue #1
 reversedLocations = sorted(recordsToWrite.keys(), reverse=True)
 
 templateVars = {
@@ -102,3 +103,9 @@ with open(workDir + "/upload/index.html", "w") as indexWriter:
     indexWriter.write(indexTemplate.render({
         "locations": sorted(recordsToWrite.keys(), reverse=True),
     }))
+
+# Write the locations
+for location in reversedLocations:
+    logger.info("Writing location: {0}".format(location))
+
+    destFile = "upload/location_{0}.html".format(location.replace(" ", ""))
