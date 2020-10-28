47265f9c247f93ca340ca92bf28caa77d83ccfbe
Sebastian Elisa Pfeifer (via Travis CI) <sebastian.pfeifer@unicorncloud.org>
2020-10-28 18:59:28 +0000

    Deploy Metalab/library-opac to github.com/Metalab/library-opac.git:gh-pages

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("#navbar-burger").click(function(){$("#navbar-burger").toggleClass("is-active"),$("#mainNav").toggleClass("is-block")}),$(".navbar-item").click(function(){$("#mainNav").removeClass("is-block")})});
+$(document).ready((function(){$("#navbar-burger").click((function(){$("#navbar-burger").toggleClass("is-active"),$("#mainNav").toggleClass("is-block")})),$(".navbar-item").click((function(){$("#mainNav").removeClass("is-block")}))}));
