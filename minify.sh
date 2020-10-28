7e52852f3c84af42e31df28f969bdf2b93682a69
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2020-10-28 17:38:21 +0100

    css source map

--- minify.sh
+++ minify.sh
@@ -10,3 +10,4 @@ find upload/ -type f \
     -exec minify {} --type html -o {} \;
 
 uglifycss upload/style.css --output upload/style.css
+echo "\n/*# sourceMappingURL=style.css.map */" >> upload/style.css
