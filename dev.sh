f3f2482756cb50971ecfc72e622b2f85a360ad88
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2020-11-01 18:53:15 +0100

    dev

--- dev.sh
+++ dev.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 # Sebastian Elisa Pfeifer <sebastian@sebastian-elisa-pfeifer.eu>
 
+set -e
+
 sourceRepo="https://github.com/Metalab/library-opac.git"
 targetFolder="/var/www/html/sebastian/metalab-library-opac/branches"
 sourceFolder="/var/www/html/sebastian/metalab-library-opac/source"
+
+sudo apt-get install -y golang-go rsync minify npm > /dev/null
+sudo npm install --global terser sass > /dev/null
+export GOPATH=/tmp/go && go get github.com/Clever/csvlint/cmd/csvlint
 
 # If the repo is not present, clone it from github, otherwise do a git pull
 if [ -d ${sourceFolder} ]; then
   echo "Repo is already cloned. Getting latest version..."
   cd ${sourceFolder}
   git pull
+  ./update.sh
 else
   echo "Repo is not clonded."
   git clone ${sourceRepo} ${sourceFolder}
+  cd ${sourceFolder}
+  ./update.sh
 fi
+
+/tmp/go/bin/csvlint /tmp/library-media-inventory/inventory.csv
 
 # For every branch, create a folder, delete the rest
 rm -rf ${targetFolder}
@@ -26,5 +37,8 @@ for branch in $(< /tmp/branches); do
   rsync -avP "${sourceFolder}/" "${targetFolder}/${branch}/"
   cd ${targetFolder}/${branch}
   git checkout ${branch}
+  sudo pip3 install -r requirements.txt > /dev/null
+  rsync -av --info=progress2 --delete ./static/ ./upload/
+  ./staticSiteGenerator.py
   cd ..
 done
