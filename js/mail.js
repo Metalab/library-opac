7af1c1394de06cd1df4ee3f06d7e0fe379006b3b
Sebastian Elisa Pfeifer (via Travis CI) <sebastian.pfeifer@unicorncloud.org>
2020-11-08 19:50:14 +0000

    Deploy Metalab/library-opac to github.com/Metalab/library-opac.git:gh-pages

---
+++ js/mail.js
@@ -0,0 +1,2 @@
+function UnCryptMailto(t){for(var n=0,o="",r=0;r<t.length;r++)(n=t.charCodeAt(r))>=8364&&(n=128),o+=String.fromCharCode(n-1);return o}function linkTo_UnCryptMailto(t){location.href=UnCryptMailto(t)}
+//# sourceMappingURL=mail.js.map
