ba12070db12294d1762d35661a26e996d1013171
Sebastian Elisa Pfeifer (via Travis CI) <sebastian.pfeifer@unicorncloud.org>
2020-11-30 21:19:49 +0000

    Deploy bibliothek.metalab.ac.at to github.com/Metalab/library-opac.git:gh-pages

---
+++ js/backToTop.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){var o=$("#backToTop");$(window).scroll((function(){$(window).scrollTop()>300?o.addClass("show"):o.removeClass("show")})),o.on("click",(function(o){o.preventDefault(),$("html, body").animate({scrollTop:0},"500")}))}));
+//# sourceMappingURL=backToTop.js.map
